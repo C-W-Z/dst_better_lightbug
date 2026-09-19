39ba8850e8529519301c6d7b7fe6388ff952c048
C-W-Z <cwz.me@proton.me>
2026-09-19 17:38:30 +0800

    update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ local modid = 'betterlb'
 local LANGS = {
     ['zh'] = {
         name = '更好的球状光虫',
-        description = '详细功能请查看模组设置',
+        description = '详细功能请查看模组设置。\n\n兼容以下模組：\n- 沃姆伍德大修：月裔萌芽\n- 沃姆伍德全面重做！\n\n無法調整「数值怪沃姆伍德」模組的沃姆伍德光蟲',
         config = {
             { modid .. '_migrate', '光虫跟随上下地洞', '跟随的光虫会随你上下地洞', true, {
                 { '禁用', false },
@@ -21,7 +21,7 @@ local LANGS = {
                 { '启用', true },
             } },
             { modid .. '_hp', '光虫血量', '调整光虫血量', false, {
-                { '禁用', false, '若你希望使用其他模組的設定可以選' },
+                { '禁用', false, '若你希望使用其他模组的设定可以选' },
                 { '25（原版）', 25 },
                 { '50', 50 },
                 { '100', 100 },
@@ -29,7 +29,7 @@ local LANGS = {
                 { '500', 500 },
                 { '1000', 1000 },
             } },
-            { modid .. '_regen_hp', '每秒回血量', '调整光虫每秒自動回復最大生命值多少比例的血量', 0, {
+            { modid .. '_regen_hp', '每秒回血量', '调整光虫每秒自动回复最大生命值多少比例的血量', 0, {
                 { '禁用', 0 },
                 { '1%', 0.01 },
                 { '2%', 0.02 },
@@ -41,7 +41,7 @@ local LANGS = {
                 { '50%', 0.5 },
             } },
             { modid .. '_light_range', '光虫光照范围', '调整光虫照明半径', false, {
-                { '禁用', false, '若你希望使用其他模組的設定可以選' },
+                { '禁用', false, '若你希望使用其他模组的设定可以选' },
                 { '1x', 1 },
                 { '1.5x', 1.5 },
                 { '2x', 2 },
@@ -50,8 +50,8 @@ local LANGS = {
                 { '4x', 4 },
                 { '5x', 5 },
             } },
-            { modid .. '_max_follow', '最大跟隨數量', '调整最多可以有幾隻光蟲跟隨玩家', false, {
-                { '禁用', false, '若你希望使用其他模組的設定可以選' },
+            { modid .. '_max_follow', '最大跟随数量', '调整最多可以有几只光虫跟随玩家', false, {
+                { '禁用', false, '若你希望使用其他模组的设定可以选' },
                 { '1', 1 },
                 { '2', 2 },
                 { '3（原版）', 3 },
@@ -62,7 +62,7 @@ local LANGS = {
                 { '8', 8 },
                 { '9', 9 },
             } },
-            -- { modid .. '_wander_range', '野生光蟲遊蕩範圍', '调整野生光蟲遊蕩半徑', 10, {
+            -- { modid .. '_wander_range', '野生光虫游荡范围', '调整野生光虫游荡半径', 10, {
             --     { '0', 0 },
             --     { '1', 1 },
             --     { '2', 2 },
@@ -74,8 +74,8 @@ local LANGS = {
             --     { '20', 20 },
             -- } },
             { '沃姆伍德的光虫' },
-            { modid .. '_w_lifetime', '光虫存活時間', '', 10, {
-                { '禁用', false, '若你希望使用其他模組的設定可以選' },
+            { modid .. '_w_lifetime', '光虫存活时间', '', 10, {
+                { '禁用', false, '若你希望使用其他模组的设定可以选' },
                 { '1天', 1 },
                 { '2天', 2 },
                 { '2.5天（原版）', 2.5 },
@@ -93,8 +93,8 @@ local LANGS = {
                 { '禁用', false },
                 { '启用', true },
             } },
-            { modid .. '_w_hp', '光虫血量', '調整光虫血量', false, {
-                { '禁用', false, '若你希望使用其他模組的設定可以選' },
+            { modid .. '_w_hp', '光虫血量', '调整光虫血量', false, {
+                { '禁用', false, '若你希望使用其他模组的设定可以选' },
                 { '25（原版）', 25 },
                 { '50', 50 },
                 { '100', 100 },
@@ -102,7 +102,7 @@ local LANGS = {
                 { '500', 500 },
                 { '1000', 1000 },
             } },
-            { modid .. '_w_regen_hp', '每秒回血量', '调整光虫每秒自動回復最大生命值多少比例的血量', 0, {
+            { modid .. '_w_regen_hp', '每秒回血量', '调整光虫每秒自动回复最大生命值多少比例的血量', 0, {
                 { '禁用', 0 },
                 { '1%', 0.01 },
                 { '2%', 0.02 },
@@ -114,7 +114,7 @@ local LANGS = {
                 { '50%', 0.5 },
             } },
             { modid .. '_w_light_range', '光虫光照范围', '调整光虫照明半径', false, {
-                { '禁用', false, '若你希望使用其他模組的設定可以選' },
+                { '禁用', false, '若你希望使用其他模组的设定可以选' },
                 { '1x', 1 },
                 { '1.5x', 1.5 },
                 { '2x', 2 },
@@ -123,8 +123,8 @@ local LANGS = {
                 { '4x', 4 },
                 { '5x', 5 },
             } },
-            { modid .. '_w_max_follow', '最大跟隨數量', '调整最多可以有幾隻光蟲跟隨玩家', false, {
-                { '禁用', false, '若你希望使用其他模組的設定可以選' },
+            { modid .. '_w_max_follow', '最大跟随数量', '调整最多可以有几只光虫跟随玩家', false, {
+                { '禁用', false, '若你希望使用其他模组的设定可以选' },
                 { '1', 1 },
                 { '2', 2 },
                 { '3', 3 },
@@ -139,16 +139,10 @@ local LANGS = {
     },
     ['en'] = {
         name = 'Better Bulbous Lightbug',
-        description = 'For details, please see the mod settings.',
+        description = 'For details, please see the mod settings.\n\nCompatible with the following mods:\n- Wormwood Overhaul - Moon Sprout\n- Wormwood Rework ！\n\nCannot adjust Wormwood\'s Lightbug in the "The Value Monster: Wormwood" mod.',
         config = {
-            -- { 'LANGUAGE' },
-            -- { modid .. '_lang', 'language', 'choose language', 'en', {
-            --     { '简体中文', 'cn' },
-            --     { 'English', 'en' }
-            -- } },
-            -- { 'FUNCTIONS' },
-            { modid .. '_migrate', 'Lightbug Caves Migration',
-                'Following Lightbugs will travel with you between caves and surface', true, {
+            { modid .. '_migrate', 'Lightbugs Follow Through Worlds',
+                'Following Lightbugs will travel with you between caves and surface.', true, {
                 { 'Disabled', false },
                 { 'Enabled',  true },
             } },
@@ -160,7 +154,8 @@ local LANGS = {
                 { 'Disabled', false },
                 { 'Enabled',  true },
             } },
-            { modid .. '_hp', 'Lightbug Health', 'Adjust Lightbug max health', 25, {
+            { modid .. '_hp', 'Lightbug Health', 'Adjust the max health of Lightbugs.', false, {
+                { 'Disabled',     false, 'Select this if you want to use settings from other mods.' },
                 { '25 (Vanilla)', 25 },
                 { '50',           50 },
                 { '100',          100 },
@@ -168,35 +163,100 @@ local LANGS = {
                 { '500',          500 },
                 { '1000',         1000 },
             } },
-            { modid .. '_regen_hp', 'Health Regen per second',
-                'Adjust the percentage of maximum health that Lightbugs automatically regenerate per second', 0, {
-                { '0% (Vanilla)', 0 },
-                { '1%',           0.01 },
-                { '2%',           0.02 },
-                { '3%',           0.03 },
-                { '4%',           0.04 },
-                { '5%',           0.05 },
-                { '10%',          0.1 },
-                { '25%',          0.25 },
-                { '50%',          0.5 },
+            { modid .. '_regen_hp', 'Health Regen Per Second',
+                'Adjust the percentage of max health Lightbugs regenerate per second.', 0, {
+                { 'Disabled', 0 },
+                { '1%',       0.01 },
+                { '2%',       0.02 },
+                { '3%',       0.03 },
+                { '4%',       0.04 },
+                { '5%',       0.05 },
+                { '10%',      0.1 },
+                { '25%',      0.25 },
+                { '50%',      0.5 },
             } },
-            { modid .. '_light_range', 'Light Radius Multiplier', 'Adjust Lightbug light radius', 1, {
-                { '1x',   1 },
-                { '1.5x', 1.5 },
-                { '2x',   2 },
-                { '2.5x', 2.5 },
-                { '3x',   3 },
-                { '4x',   4 },
-                { '5x',   5 },
+            { modid .. '_light_range', 'Light Radius', 'Adjust the illumination radius of Lightbugs.', false, {
+                { 'Disabled', false, 'Select this if you want to use settings from other mods.' },
+                { '1x',       1 },
+                { '1.5x',     1.5 },
+                { '2x',       2 },
+                { '2.5x',     2.5 },
+                { '3x',       3 },
+                { '4x',       4 },
+                { '5x',       5 },
             } },
-            { modid .. '_max_follow', 'Maximum Following Lightbugs',
-                'Adjust the maximum number of Lightbugs that can follow you', 3, {
+            { modid .. '_max_follow', 'Max Followers', 'Adjust the maximum number of Lightbugs that can follow a player.', false, {
+                { 'Disabled',    false, 'Select this if you want to use settings from other mods.' },
                 { '1',           1 },
                 { '2',           2 },
                 { '3 (Vanilla)', 3 },
                 { '4',           4 },
                 { '5',           5 },
                 { '6',           6 },
+                { '7',           7 },
+                { '8',           8 },
+                { '9',           9 },
+            } },
+            { 'Wormwood\'s Lightbugs' },
+            { modid .. '_w_lifetime', 'Lightbug Lifetime', '', 10, {
+                { 'Disabled',           false, 'Select this if you want to use settings from other mods.' },
+                { '1 Day',              1 },
+                { '2 Days',             2 },
+                { '2.5 Days (Vanilla)', 2.5 },
+                { '5 Days',             5 },
+                { '7.5 Days',           7.5 },
+                { '10 Days',            10 },
+                { '15 Days',            15 },
+                { '20 Days',            20 },
+                { '25 Days',            25 },
+                { '30 Days',            30 },
+                { '50 Days',            50 },
+                { '100 Days',           100 },
+            } },
+            { modid .. '_w_invincible', 'Invincible Lightbugs', '', true, {
+                { 'Disabled', false },
+                { 'Enabled',  true },
+            } },
+            { modid .. '_w_hp', 'Lightbug Health', 'Adjust the max health of Lightbugs.', false, {
+                { 'Disabled',     false, 'Select this if you want to use settings from other mods.' },
+                { '25 (Vanilla)', 25 },
+                { '50',           50 },
+                { '100',          100 },
+                { '250',          250 },
+                { '500',          500 },
+                { '1000',         1000 },
+            } },
+            { modid .. '_w_regen_hp', 'Health Regen Per Second',
+                'Adjust the percentage of max health Lightbugs regenerate per second.', 0, {
+                { 'Disabled', 0 },
+                { '1%',       0.01 },
+                { '2%',       0.02 },
+                { '3%',       0.03 },
+                { '4%',       0.04 },
+                { '5%',       0.05 },
+                { '10%',      0.1 },
+                { '25%',      0.25 },
+                { '50%',      0.5 },
+            } },
+            { modid .. '_w_light_range', 'Light Radius', 'Adjust the illumination radius of Lightbugs.', false, {
+                { 'Disabled', false, 'Select this if you want to use settings from other mods.' },
+                { '1x',       1 },
+                { '1.5x',     1.5 },
+                { '2x',       2 },
+                { '2.5x',     2.5 },
+                { '3x',       3 },
+                { '4x',       4 },
+                { '5x',       5 },
+            } },
+            { modid .. '_w_max_follow', 'Max Followers',
+                'Adjust the maximum number of Lightbugs that can follow Wormwood.', false, {
+                { 'Disabled',    false, 'Select this if you want to use settings from other mods.' },
+                { '1',           1 },
+                { '2',           2 },
+                { '3',           3 },
+                { '4',           4 },
+                { '5',           5 },
+                { '6 (Vanilla)', 6 },
                 { '7',           7 },
                 { '8',           8 },
                 { '9',           9 },
